5a621e505c2699b0263715f4b645bf269245d1fa
Luiz <luizgonzagafm1997@gmail.com>
2025-04-09 17:37:22 -0300

    update project

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -8,10 +8,13 @@ const Intro = () => {
       <section id="intro" className="intro">
         <img src={introPhoto} alt="Foto de Luiz Gonzaga" className="intro__photo" />
         <div className="intro__titles">
-          <h1 className="intro__title">Luiz Gonzaga</h1>
+          <h1 className="intro__title">&lt;Luiz Gonzaga/&gt;</h1>
           <h2 className="intro__subtitle">Desenvolvedor Front-end & Designer</h2>
         </div>
-        <p className="intro__details">Nascido em 1997 e desde cedo movido pela paixão por tecnologia. Sempre fui fascinado por tudo que envolve inovação — de celulares e videogames até computadores e gadgets que transformam nosso dia a dia.</p>
+        <p className="intro__details">
+          Nascido em 1997 e desde cedo movido pela paixão por tecnologia. Sempre fui fascinado por tudo que envolve inovação — de celulares e videogames até computadores e gadgets que transformam
+          nosso dia a dia.
+        </p>
         <a href="./src/assets/docs/profile.pdf" className="intro__button" download>
           Download CV
         </a>
